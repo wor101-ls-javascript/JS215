f5429de7f77149b1ab58304bed2cd4fb9ac349b7
Will <wor101@gmail.com>
2021-09-08 11:39:51 -0400

    more abstraction practice

--- classRecordsSummary.js
+++ classRecordsSummary.js
@@ -110,19 +110,115 @@ let studentScores = {
 
 function generateClassRecordSummary(studentScores) {
   let studentGrades = finalScoresAndLetterGrades(studentScores);
+  let examMetrics = calculateExamMetrics(studentScores);
+  return { 'studentGrades': studentGrades, exams: examMetrics };
 }
+
+function calculateExamMetrics(studentScores) {
+  let allStudentsExams = getStudentsExamScores(studentScores);
+  let allExamScores = getEachExamsScores(allStudentsExams);
+  let averageExamScores = getAverageExamScores(allExamScores);  // [ 75.6, 86.4, 87.6, 91.8 ] - array of average score for each exam
+  let minimumExamScores = getMinimumExamScores(allExamScores);  // [ 50, 70, 60, 80 ] - array of minimum scores for each exam
+  let maximumExamScores = getMaximumExamScores(allExamScores);  // [ 100, 100, 100, 100 ] - array of maximum scores for each exam
+  let arrayOfExamMetrics = constructArrayOfExamMetrics(averageExamScores, minimumExamScores, maximumExamScores);
+  return arrayOfExamMetrics;
+}
+
+function constructArrayOfExamMetrics(averageExamScores, minimumExamScores, maximumExamScores) {
+  return averageExamScores.map((averageScore, index) => {
+    return { average: averageScore, minimum: minimumExamScores[index], maximum: maximumExamScores[index]};
+  });
+}
+
+function getMaximumExamScores(allExamScores) {
+  return allExamScores.map(singleExamScores => Math.max(...singleExamScores));
+}
+
+function getMinimumExamScores(allExamScores) {
+  return allExamScores.map(singleExamScores => Math.min(...singleExamScores));
+}
+
+function getAverageExamScores(allExamScores) {
+  return allExamScores.map(singleExamScores => getAverage(singleExamScores));
+}
+
+function getEachExamsScores(allStudentsExams) {
+  let exam1 = allStudentsExams.map(student => student[0]);      // [ 90, 50, 88, 100, 50 ] - all scores for exam1
+  let exam2 = allStudentsExams.map(student => student[1]);      // [ 95, 70, 87, 100, 80 ] - all scores for exam2
+  let exam3 = allStudentsExams.map(student => student[2]);      // [ 100, 90, 88, 100, 60 ] - all scores for exam3
+  let exam4 = allStudentsExams.map(student => student[3]);      // [ 80, 100, 89, 100, 90 ] - all scores for exam4
+  return [exam1, exam2, exam3, exam4];
+}
+
+
 
 function finalScoresAndLetterGrades(studentScores) {
   let studentsExamScores = getStudentsExamScores(studentScores);
   let averageScores = getAverageScores(studentsExamScores);
+  // console.log(averageScores);                                                               // [ 91, 78, 88, 100, 70 ] - average exam scores for each student
 
   let studentExcerciseScores = getStudentsExcerciseScores(studentScores);
-  console.log(studentExcerciseScores);
+  let studentsTotalExcerciseScore = totalStudentExcerciseScores(studentExcerciseScores);
+  // console.log(studentsTotalExcerciseScore);                                                 // [ 79, 65, 77, 60, 30 ] - totaled excercise scores for each student
+
+  let weightedGrades = calculateWeightedGrade(averageScores, studentsTotalExcerciseScore);
+  // console.log(weightedGrades);                                                              // [ 87, 73, 84, 86, 56 ] - combined weighted grades for each student
+
+  let letterGrades = getLetterGrades(weightedGrades);
+  // console.log(letterGrades);                                                                // [ 'B', 'D', 'C', 'B', 'F' ] - letter grades for each student
+
+  let combinedNumberAndLetterGrades = combineNumberAndLetterGrades(weightedGrades, letterGrades);
+  // console.log(combinedNumberAndLetterGrades);                                               // [ '87 (B)', '73 (D)', '84 (C)', '86 (B)', '56 (F)' ] - combined percent/letter grades for each student
+
+  return combinedNumberAndLetterGrades;
+}
+
+function combineNumberAndLetterGrades(weightedGrades, letterGrades) {
+  let combinedGrades = weightedGrades.map((percentGrade, index) => {
+    return `${percentGrade} (${letterGrades[index]})`;
+  });
+  return combinedGrades;
+}
+
+function getLetterGrades(grades) {
+  let letterGrades = grades.map(grade => {
+                       if (grade >= 93) {
+                         return 'A';
+                       } else if (grade >= 85) {
+                         return 'B';
+                       } else if (grade >= 77) {
+                         return 'C';
+                       } else if (grade >= 69) {
+                         return 'D';
+                       } else if (grade >= 60) {
+                         return 'E';
+                       } else {
+                         return 'F';
+                       }
+                     });
+  return letterGrades;
+}
+
+function calculateWeightedGrade(examScores, excerciseScores) {
+  const examWeight = 0.65;
+  const excerciseWeight = 0.35;
+  let weightedExamScores = examScores.map(score => score * examWeight);
+  let weightedExcerciseScores = excerciseScores.map(score => score * excerciseWeight);
+  let combinedScores = weightedExamScores.map((examScore, index) =>  {
+                         return Math.round(examScore + weightedExcerciseScores[index]);
+                       });
+  return combinedScores;
 }
 
 function getStudentsExcerciseScores(studentScores) {
   let students = Object.values(studentScores);
   return students.map(student => student.scores.exercises);
+}
+
+function totalStudentExcerciseScores(studentExcerciseScores) {
+  return studentExcerciseScores.map(studentScores => {
+    return studentScores.reduce((total, score) => total + score)
+  });
 }
 
 function getAverageScores(studentsExamScores) {
@@ -143,7 +239,7 @@ function extractExamScores(student){
   return student.scores.exams;
 }
 
-generateClassRecordSummary(studentScores);
+console.log(generateClassRecordSummary(studentScores));
 
 // returns:
 // {
